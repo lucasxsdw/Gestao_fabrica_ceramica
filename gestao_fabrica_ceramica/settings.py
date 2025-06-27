7cb45d9cad25a0502a4bb4b5a9cf0294a6811ecd
natanfrota <natanfrota31@gmail.com>
2025-06-27 10:00:52 -0300

    fix: corrigi a versão do Bootstrap

--- gestao_fabrica_ceramica/settings.py
+++ gestao_fabrica_ceramica/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     "crispy_forms",
-    "crispy_bootstrap5",
+    "crispy_bootstrap4",
 
     'funcionario',
 ]
@@ -115,9 +115,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
-CRISPY_TEMPLATE_PACK = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
