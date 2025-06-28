846244ea9d1cd5a1284ae7c6e9f7c4f8bedd6455
SanchoArruda <gucairesarruda@gmail.com>
2025-06-28 16:28:22 -0300

    feat: add model

--- pagamento/admin.py
+++ pagamento/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+from django.contrib import admin # importa o admin do django, para aparecer os models no painel de administração.
+from .models import Pagamento # Importa o model Pagamento, que será registrado no admin.
 
-# Register your models here.
+@admin.register(Pagamento) # Decorador para registrar o model Pagamento no admin do Django.
+class PagamentoAdmin(admin.ModelAdmin): # Classe que define como o model Pagamento será exibido no painel de administração do Django.
+    list_display = ('funcionario', 'data_pagamento', 'valor_total', 'vale_desconto', 'bonus_comissao') # Campos que serão exibidos na lista de pagamentos no admin.
+    list_filter = ('data_pagamento', 'funcionario') # Campos que serão usados para filtrar os pagamentos no admin.
+    search_fields = ('funcionario__nome',) # Campos que serão pesquisáveis no admin, permitindo buscar pagamentos pelo nome do funcionário.
--- pagamento/models.py
+++ pagamento/models.py
@@ -1,12 +1,12 @@
 from django.db import models #trás todos os tipos de campos que será usado(CharField, DateField, DecimalField, etc.)
 from funcionario.models import Funcionario # Importa o model Funcionario, para criar um viculo ou relaciomento.
 
-class Pagamento(models.Model): #
-    funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE, related_name='pagamentos')
-    data_pagamento = models.DateField()
-    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
-    vale_desconto = models.DecimalField(max_digits=10, decimal_places=2)
-    bonus_comissao = models.DecimalField(max_digits=10, decimal_places=2)
+class Pagamento(models.Model): # Toda classe que herda de models.Model, vira uma tabela no banco de dados.
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE, related_name='pagamentos') # Cria um relacionamento com o model Funcionario, onde cada pagamento está vinculado a um funcionário.
+    data_pagamento = models.DateField() # Campo para armazenar a data do pagamento.
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2) # Campo para armazenar o valor total do pagamento, com até 10 dígitos no total e 2 casas decimais.
+    vale_desconto = models.DecimalField(max_digits=10, decimal_places=2) # Campo para armazenar o valor do vale desconto, com até 10 dígitos no total e 2 casas decimais.
+    bonus_comissao = models.DecimalField(max_digits=10, decimal_places=2) # Campo para armazenar o valor do bônus ou comissão, com até 10 dígitos no total e 2 casas decimais.
 
-    def __str__(self):
+    def __str__(self): # Método especial que o django usa para exibir o objeto como string(texto).
         return f"{self.funcionario.nome} - {self.data_pagamento} - R$ {self.valor_total}"
