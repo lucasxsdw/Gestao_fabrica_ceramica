da52710985abb33dbef8f168aa82c2ac1a3727a1
natanfrota <natanfrota31@gmail.com>
2025-06-29 12:45:47 -0300

    feat: adiciona página de listagem e de formulário de materiais

--- static/js/global.js
+++ static/js/global.js
@@ -1,3 +1,7 @@
-$(document).ready(function() {
-  $('.table').DataTable();
+$(document).ready(function () {
+  $('.table').DataTable({
+    language: {
+        url: '//cdn.datatables.net/plug-ins/2.3.2/i18n/pt-BR.json',
+    }
+  });
 });
