b09fee0ebeec75651fd98c4dc8baf59fdc82dd1b
natanfrota <natanfrota31@gmail.com>
2025-07-01 16:05:30 -0300

    fix: corrige os nomes das urls

--- emprestimo/urls.py
+++ emprestimo/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'emprestimo'
+
 
 urlpatterns = [
-    path('', views.listar, name='listar'),
-    path('<int:id>', views.detalhar, name='detalhar'),
-    path('adicionar/', views.adicionar, name='adicionar'),
-    path('editar/<int:id>', views.editar, name='editar'),
-    path('excluir/<int:id>', views.excluir, name='excluir'),
+    path('', views.listar, name='listar_emprestimos'),
+    path('<int:id>', views.detalhar, name='detalhar_emprestimo'),
+    path('adicionar/', views.adicionar, name='adicionar_emprestimo'),
+    path('editar/<int:id>', views.editar, name='editar_emprestimo'),
+    path('excluir/<int:id>', views.excluir, name='excluir_emprestimo'),
 ]
